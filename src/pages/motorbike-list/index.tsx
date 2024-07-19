dbeb7e334a779632d0ad27943b128e5ae84fa33b
Hung <lamgiahung112@gmail.com>
2024-07-19 21:28:09 +0700

    fix motorbike list item

--- src/pages/motorbike-list/index.tsx
+++ src/pages/motorbike-list/index.tsx
@@ -61,15 +61,21 @@ function MotorbikeListPage() {
                     XE TAY GA
                 </div>
                 <div className="flex flex-wrap gap-x-8 justify-between">
-                    <div className="flex flex-col w-full md:w-[40%] lg:w-[30%] bg-neutral-200 p-8">
-                        <div className="text-xl font-semibold">LEXI</div>
-                        <div className="">LEXI</div>
-                    </div>
-                    <div className="flex w-full md:w-[40%] lg:w-[30%]">
-                    ss
-                    </div>
-                    <div className="flex w-full md:w-[40%] lg:w-[30%]">
-                        ss
+                    <div className="flex flex-col w-full md:w-[40%] lg:w-[30%] bg-neutral-200 p-8 hover:bg-neutral-100 cursor-pointer">
+                        <div className="text-2xl font-semibold">LEXI</div>
+                        <div className="flex gap-x-1">Giá từ<span className="font-semibold">48.500.000 VNĐ</span></div>
+                        <div className="line-clamp-3 text-neutral-500 text-sm leading-6 w-full text-ellipsis whitespace-normal">
+                            Xuất phát từ khái niệm "kết hợp phong cách thể thao và sự tinh tế thời thượng", LEXI 2024 hoàn toàn mới mang lại sức hấp dẫn khó cưỡng với kiểu dáng hiện đại và sang trọng, khả năng kết nối điện thoại thông minh tiện lợi, sức mạnh khối động cơ 155cc cho công suất và hiệu suất vượt trội so với những khối động cơ cùng dung tích.
+                        </div>
+                        <img
+                            alt=""
+                            src="https://yamaha-motor.com.vn/wp/wp-content/uploads/2024/06/Lexi-155_Mat-Black-4-768x645.png"
+                        />
+                        <div className="flex gap-x-4 justify-center items-center">
+                            <div className="w-4 h-4 rounded-full bg-amber-300"></div>
+                            <div className="w-4 h-4 rounded-full bg-slate-700"></div>
+                            <div className="w-4 h-4 rounded-full bg-red-500"></div>
+                        </div>
                     </div>
                 </div>
             </div>
